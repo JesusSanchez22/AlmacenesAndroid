aeb3f98d0fe0d4230b34163f9aa3ecd350f8a1f3
JesusSanchez22 <jessanram22@gmail.com>
2023-12-11 14:12:05 +0100

    11/12-jesus-clase

--- app/src/main/java/com/example/almaceneskikoandroid/MainActivity.java
+++ app/src/main/java/com/example/almaceneskikoandroid/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.almaceneskikoandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menuopciones,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id==R.id.opcionProductos){
+
+            Intent intent = new Intent(MainActivity.this, ProductosActivity.class);
+            startActivity(intent);
+
+        }
+
+        if(id==R.id.opcionClientes){
+
+
+        }
+
+        if(id==R.id.opcionMapa){
+            Toast.makeText(this, "Se seleccionó la tercera opción", Toast.LENGTH_SHORT).show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
