4d773e12934d9e62e969442ca074b4a8dabf0b87
DAM <raul.percal@educa.jcyl.es>
2023-12-13 12:31:54 +0100

    Segundo commit clase, checkbox

--- app/src/main/java/com/example/almaceneskikoandroid/login.java
+++ app/src/main/java/com/example/almaceneskikoandroid/login.java
@@ -3,7 +3,10 @@ package com.example.almaceneskikoandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.example.almaceneskikoandroid.functions.Funciones;
@@ -12,15 +15,16 @@ import com.example.almaceneskikoandroid.functions.Funciones;
 
 public class login extends AppCompatActivity {
 
+    private CheckBox checkBox;
     private EditText etUserLogin, etPasswordLogin;
 
     //Perfil 1
-    private String user1 = "profesorIES";
-    private String user2 = "123";
+    private String user1 = "raul";
+    private String passwordUser1 = "123";
 
     //Perfil 2
-    private String usuarioAlumno = "alumnoIES";
-    private String contrasenaAlumno = "abc";
+    private String user2 = "jesus";
+    private String passwordUser2 = "123";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +34,26 @@ public class login extends AppCompatActivity {
         etUserLogin = findViewById(R.id.etUserLogin);
         etPasswordLogin = findViewById(R.id.etPasswordLogin);
 
+        checkBox = findViewById(R.id.checkBox);
+
+        checkBox.setChecked(false);
+
     }
+
+    public void ShowPassword(View v) {
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    etPasswordLogin.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    etPasswordLogin.setInputType(129);
+                }
+            }
+        });
+    }
+
+
 
     public void verificar(View v){
 
@@ -41,9 +64,11 @@ public class login extends AppCompatActivity {
             Funciones.mostrarToastCorto(this,"No has metido ningún usuario");
         } else if ((contrasenaIn.isEmpty())) {
             Funciones.mostrarToastCorto(this,"La contraseña no puede estar vacía");
-        } else if (usuarioIn.equals(user1) && contrasenaIn.equals(user2)){
+        } else if (usuarioIn.equals(user1) && contrasenaIn.equals(passwordUser1)){
+            Funciones.mostrarToastCorto(this,"Contraseña correcta user1");
             //cambiarActivity(.class);
-        } else if (usuarioIn.equals(usuarioAlumno) && contrasenaIn.equals(contrasenaAlumno)){
+        } else if (usuarioIn.equals(user2) && contrasenaIn.equals(passwordUser2)){
+            Funciones.mostrarToastCorto(this,"Contraseña correcta user2");
             //cambiarActivity(.class);
         } else {
             Funciones.mostrarToastCorto(this, "Usuario o Contraseña Incorrectos");
